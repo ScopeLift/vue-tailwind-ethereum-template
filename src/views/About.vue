8e32a96f95391fe0a071574a81273de44f0b3758
Matt Solomon <matt@mattsolomon.dev>
2021-05-29 18:08:15 -0700

    Setup tailwind

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,98 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h3 class="text-lg leading-6 font-medium text-gray-900">Last 30 days</h3>
+
+    <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+      <div
+        v-for="item in stats"
+        :key="item.id"
+        class="relative bg-white pt-5 px-4 pb-12 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden"
+      >
+        <dt>
+          <div class="absolute bg-indigo-500 rounded-md p-3">
+            <component :is="item.icon" class="h-6 w-6 text-white" aria-hidden="true" />
+          </div>
+          <p class="ml-16 text-sm font-medium text-gray-500 truncate">{{ item.name }}</p>
+        </dt>
+        <dd class="ml-16 pb-6 flex items-baseline sm:pb-7">
+          <p class="text-2xl font-semibold text-gray-900">
+            {{ item.stat }}
+          </p>
+          <p
+            :class="[
+              item.changeType === 'increase' ? 'text-green-600' : 'text-red-600',
+              'ml-2 flex items-baseline text-sm font-semibold',
+            ]"
+          >
+            <ArrowSmUpIcon
+              v-if="item.changeType === 'increase'"
+              class="self-center flex-shrink-0 h-5 w-5 text-green-500"
+              aria-hidden="true"
+            />
+            <ArrowSmDownIcon
+              v-else
+              class="self-center flex-shrink-0 h-5 w-5 text-red-500"
+              aria-hidden="true"
+            />
+            <span class="sr-only">
+              {{ item.changeType === 'increase' ? 'Increased' : 'Decreased' }} by
+            </span>
+            {{ item.change }}
+          </p>
+          <div class="absolute bottom-0 inset-x-0 bg-gray-50 px-4 py-4 sm:px-6">
+            <div class="text-sm">
+              <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+                View all<span class="sr-only"> {{ item.name }} stats</span></a
+              >
+            </div>
+          </div>
+        </dd>
+      </div>
+    </dl>
   </div>
 </template>
+
+<script>
+import { defineComponent } from 'vue';
+import { ArrowSmDownIcon, ArrowSmUpIcon } from '@heroicons/vue/solid';
+import { CursorClickIcon, MailOpenIcon, UsersIcon } from '@heroicons/vue/outline';
+
+const stats = [
+  {
+    id: 1,
+    name: 'Total Subscribers',
+    stat: '71,897',
+    icon: UsersIcon,
+    change: '122',
+    changeType: 'increase',
+  },
+  {
+    id: 2,
+    name: 'Avg. Open Rate',
+    stat: '58.16%',
+    icon: MailOpenIcon,
+    change: '5.4%',
+    changeType: 'increase',
+  },
+  {
+    id: 3,
+    name: 'Avg. Click Rate',
+    stat: '24.57%',
+    icon: CursorClickIcon,
+    change: '3.2%',
+    changeType: 'decrease',
+  },
+];
+export default defineComponent({
+  components: {
+    ArrowSmDownIcon,
+    ArrowSmUpIcon,
+  },
+  setup() {
+    return {
+      stats,
+    };
+  },
+});
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  // Default theme: https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/defaultConfig.stub.js#L7
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
