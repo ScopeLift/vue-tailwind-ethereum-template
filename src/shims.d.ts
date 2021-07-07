527c892918945a13465c869f38614dc5b406849c
Matt Solomon <matt@mattsolomon.dev>
2021-07-07 11:53:29 -0700

    Fix ESLint config and cleanup based on lint output

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,7 +1,7 @@
 // Shim for Vue composition API
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
-  const component: DefineComponent<{}, {}, any>;
+  const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, any>;
   export default component;
 }
 
